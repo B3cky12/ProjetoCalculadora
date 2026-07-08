80bac97c189b95f6a81cdb69019a2cb64a39f2f9
Ellen Rebeca <ellen.rebeca10@gmail.com>
2026-07-07 23:03:29 -0300

    build: create subtraction basic logic

--- src/core/operations.py
+++ src/core/operations.py
@@ -1,3 +1,7 @@
 def operation_soma(parte1, parte2):
     resultado = parte1 + parte2
     return resultado
+
+def operation_subtrai(parte1, parte2):
+    resultado = parte1 - parte2
+    return resultado
