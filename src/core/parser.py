1a0b377e995fc3a3069f1d6407b86d8c309ad08a
Ellen Rebeca <ellen.rebeca10@gmail.com>
2026-07-12 16:40:26 -0300

    build(parser): possible logic pemdas e

--- src/core/parser.py
+++ src/core/parser.py
@@ -1,3 +1,4 @@
+# quanto mais eu leio sobre parsers, mais eu penso que esse código não vai ser a versão final, mas o exercício mental é válido por hora
 def parse_nele(expressao: str):
     # A estrutura com a expressão decodificada, organizada na ordem de resolução
     parsed = []
@@ -11,12 +12,40 @@ def parse_nele(expressao: str):
             parte = parse_nele(parenteses) # Se tiver parenteses aninhados, aqui é resolvido, se não, segue normal
             parsed.append(parte)
         elif index == -1:
+            # possível teste para avaliar necessidade desse elif
             continue
     
     for index, char in enumerate(expressao):
         # Segunda regra do PEMDAS: Expoentes
         if char == "^":
             # pensamento: loop reverso iniciado no ^, pra achar index que começa o bloco do número que é a base da potenciação; depois loop com direção normal pra encontrar o index que termina o bloco de número que é o expoente;
+            
+            last = 0
+            for i, x in range(enumerate(expressao)[index-1], 0, -1):
+                now = {i: x}
+                if type(x) is int:
+                    last = {i: x}
+                    continue
+                else:
+                    break
+            base = expressao[last[i]:index-1]
+            parte1 = parse_nele(base)
+            
+            last = 0
+            for i, x in range(enumerate(expressao)[index+1]):
+                now = {i: x}
+                if type(x) is int:
+                    last = {i: x}
+                    continue
+                else:
+                    break
+            expo = expressao[index+1:last[i]]
+            parte2 = parse_nele(expo)
+            
+            parsed.append(parte1)
+            parsed.append("^")
+            parsed.append(parte2)
             # pergunta: é válido chamar parse_nele em ambas a base e o expoente?
+    
     
     return parsed
